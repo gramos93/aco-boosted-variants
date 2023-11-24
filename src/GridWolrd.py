985a1b167f69c79ced945879e1d3094f62b3b3d7
gabrieljeanson <gabrieljeanson@outlook.fr>
2023-11-24 12:25:40 -0500

    add probability and target position

--- src/GridWolrd.py
+++ src/GridWolrd.py
@@ -40,6 +40,32 @@ def plot(grid):
     colors = np.array([[255, 164, 6], [138, 181, 73], [95, 126, 48], [186, 140, 93]], dtype=np.uint8)
     image = colors[grid.reshape(-1)].reshape(grid.shape + (3,))
     plt.imshow(image)
+    
+def get_target_position(grid, n):
+    position = np.random.random_integers(n-1, size=(2,))
+    while (grid[position[0], position[1]]) != 1 and (grid[position[0], position[1]]) != 3:
+        position = np.random.random_integers(n, size=(2,))
+    return position
+
+def generate_probability_matrix(position, grid):
+    n = grid.shape[0]
+    prob_matrix = np.zeros((n, n), dtype=float)
+
+    for i in range(n):
+        for j in range(n):
+            if grid[i, j] == 1 or grid[i, j] == 3:
+                # Calculate the Euclidean distance between the current position and the target position
+                distance = np.linalg.norm(np.array([i, j]) - np.array(position))
+                # The closer the entry is to the position, the bigger the probability
+                prob = 1 / (1 + distance)
+                if (grid[i, j] == 3):
+                    prob = prob/2
+                prob_matrix[i, j] = prob
+
+    # Normalize the probability matrix to ensure the sum of probabilities is 1
+    prob_matrix /= np.sum(prob_matrix)
+
+    return prob_matrix    
 
 def getGrid(n):
 
@@ -62,11 +88,21 @@ def getGrid(n):
     mask = (grid == 1) * (np.random.rand(n, n) < 0.05)
     grid[mask] = 3
 
-    return grid
+	# Target's position
+    position = get_target_position(grid, n)
+
+    # Probability matrix
+    probability_matrix = generate_probability_matrix(position, grid)
+
+    return grid, position, probability_matrix
 
 if __name__ == '__main__':
-	grid = getGrid(32)
+    (grid, position, probability_matrix) = getGrid(32)
+    print(position)
+    print(grid)
+    print(probability_matrix)
+    print(np.sum(probability_matrix))
 
 	# Plot
-	plot(grid)
-	plt.show()
+    plot(grid)
+    plt.show()
