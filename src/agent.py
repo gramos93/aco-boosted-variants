e03ab04ee722b83efd30fb64558509832b370720
Kevin Laurent <laurent.kevin.d@gmail.com>
2023-12-12 13:03:25 -0500

    Update agent.py

--- src/agent.py
+++ src/agent.py
@@ -1,29 +1,60 @@
+import numpy as np
+
 class Agent:
     def __init__(self, idx, visibility, color, size):
         self.idx = idx
-        self.visibility = visibility
         self.current_location = (0,0)
         self.current_cost = 1
         self.path = [Node((0,0), 1)]
         self.color = color
         self.neighbors = [(0,1), (1,0)]
+        self.frontier = []
         self.size = size
         self.visited = set()
+        self.visibility = visibility
 
-    def update(self, new_location, cost):
+    def send_home(self):
+        self.current_location = (0,0)
+        self.current_cost = 1
+        self.path = [Node((0,0), 1)]
+        self.neighbors = [(0,1), (1,0)]
+        self.visited = set()
+
+    def update(self, new_location, cost, gps):
         self.current_location = new_location
-        self.current_cost = cost
+        self.current_cost += cost
         self.neighbors = []
         for i, j in [(0,1), (0,-1), (1,0), (-1,0)]:
             if 0 <= new_location[0]+i < self.size and 0 <= new_location[1]+j < self.size:
-                #if (new_location[0]+i, new_location[1]+j) not in self.visited:
-                self.neighbors.append((new_location[0]+i, new_location[1]+j))
+                if (new_location[0]+i, new_location[1]+j) not in self.visited:
+                    self.neighbors.append((new_location[0]+i, new_location[1]+j))
+                    self.frontier.append((new_location[0]+i, new_location[1]+j))
+                elif (new_location[0]+i, new_location[1]+j) in self.frontier:
+                    self.frontier.remove((new_location[0]+i, new_location[1]+j))
         self.path.append(Node(new_location, cost))
         self.visited.add(new_location)
 
+        if self.neighbors == []:
+            # search through neighbors and find the one with highest gps if agent trapped with no new neighbors
+            for i, j in [(0,1), (0,-1), (1,0), (-1,0)]:
+                if 0 <= new_location[0]+i < self.size and 0 <= new_location[1]+j < self.size:
+                    self.neighbors.append((new_location[0]+i, new_location[1]+j))
+            gps_neighbors = [gps[n[0], n[1]] for n in self.neighbors]
+            max_idx = np.argmax(gps_neighbors)
+            self.neighbors = [self.neighbors[max_idx]]
+
+
+    def get_vision(self, location):
+        vision = []
+        for i in range(-self.visibility, self.visibility+1):
+            for j in range(-self.visibility, self.visibility+1):
+                if 0 <= location[0]+i < self.size and 0 <= location[1]+j < self.size:
+                    vision.append((location[0]+i, location[1]+j))
+        return vision
+
     def get_neighbors(self):
         return self.neighbors
-
+    
     def get_location(self):
         return self.current_location
     
@@ -35,14 +66,6 @@ class Agent:
         
     def get_idx(self):
         return self.idx
-    
-    def get_visible_cases(self):
-        visible_cases = []
-        for k in range (self.visibility):
-            for i, j in [(0,1), (0,-1), (1,0), (-1,0)]:
-                if i*k <= self.size and j*k <= self.size:
-                    visible_cases.append((i*k, j*k))
-        return visible_cases
     
     def __repr__(self):
         return f'Agent {self.idx} at {self.current_location}'
